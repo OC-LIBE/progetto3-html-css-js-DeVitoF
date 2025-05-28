b8f9572b57db79ac5055b71676a49d0e6cb2c2ed
Filippo <test@gmail.com>
2025-05-28 09:43:01 +0200

    Programmare con javascript - quasi finito

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
-let croccantini = 80000;
+const website = "https://frapollif.github.io/pet-adoption-data"
+let restriction = "none"
 
-document.getElementById('cambiaColore').addEventListener('click', cambiaSfondo);
-
-function cambiaSfondo() {
-  document.body.style.backgroundColor = getRandomColor();
-  moreCroccantini();
+function sortPets(restric) {
+  restriction = restric
 }
 
-function getRandomColor() {
-  const lettere = '0123456789ABCDEF';
-  let colore = '#';
-  for (let i = 0; i < 6; i++) {
-    colore += lettere[Math.floor(Math.random() * 16)];
-  }
-  return colore;
+async function getPetsData() {
+  const data = await fetch(`${website}/pets.json`);
+  const petsdata = await data.json();
+  console.log(petsdata)
+  return petsdata
+  
 }
 
-function moreCroccantini() {
-    croccantini += 1;
+async function displayPets() {
+  const pets = await getPetsData();
+  const template = document.querySelector(`#animal-card-template`);
+  const wrapper = document.querySelector('main');
+  console.log(template)
+  pets.forEach( pet => {
+    if (pet.species == restriction || restriction == "none") {
+      // Creating clone
+      const clone = template.content.cloneNode(true);
+      // Adapting clone
+      clone.getElementById("name").textContent=`${pet.name}`;
+      clone.getElementById("birthYear").textContent=`${pet.birthYear}`;
+      clone.getElementById("description").textContent=`${pet.description}`;
+      clone.getElementById("species").textContent=`${pet.species}`;
+      clone.getElementById("button").textContent=`Adopt ${pet.name}`;
+      clone.getElementById("photo").setAttribute("src",`${pet.photo}`);
+      clone.getElementById("anchor").setAttribute("href",`${website}/pets/${pet.id}/`);
+      // Inserting clone
+      wrapper.appendChild(clone)
+    }
+    }
+  )
 }
+
+// Starting app
+displayPets()
