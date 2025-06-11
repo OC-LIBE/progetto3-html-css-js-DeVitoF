daab6be81feaeaac3df8cfa8a44dff949f38317c
Filippo <test@gmail.com>
2025-06-11 09:26:27 +0200

    Riuscita filtraggio senza reload + colorazione pulsante

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 const website = "https://frapollif.github.io/pet-adoption-data"
-let filter = "all"
 
 function onSort(filt) {
-  filter = filt
-  removePets()
-  displayPets()
+  const allClones = document.querySelectorAll(`.clone`);
+  allClones.forEach(clone =>{  
+    const speciesElem = clone.querySelector('#species');
+    console.log(speciesElem)
+    clone.classList.remove('inv', 'vis');
+    if (speciesElem.textContent.trim() == filt || filt == "All") {
+      clone.classList.add('vis');
+    } else {
+      clone.classList.add('inv');
+    }
+  });
+
+  const allButtons = document.querySelectorAll("nav button");
+  allButtons.forEach(button =>{  
+    if (button.dataset.filter == filt) {
+      button.classList.add('buttonvis');
+    } else {
+      button.classList.remove('buttonvis');
+    }
+  });
 }
 
 async function getPetsData() {
   const data = await fetch(`${website}/pets.json`);
   const petsdata = await data.json();
   return petsdata
-}
-
-function removePets() {
-  let made = document.querySelectorAll(`.clone`)
-  if (made.length > 0) {
-    made[0].remove()
-    removePets()
-  }
 }
 
 function petYears(birthYear) {
@@ -45,21 +53,20 @@ async function displayPets() {
   const template = document.querySelector(`#animal-card-template`);
   const wrapper = document.querySelector('main');
   pets.forEach( pet => {
-    if (pet.species == filter || filter == "all") {
-      // Creating clone
-      const clone = template.content.cloneNode(true);
-      clone.getElementById("template").classList.add("clone");
-      // Adapting clone
-      clone.getElementById("name").textContent=`${pet.name}`;
-      clone.getElementById("birthYear").textContent= petYears(pet.birthYear);
-      clone.getElementById("description").textContent=`${pet.description}`;
-      clone.getElementById("species").textContent= capitalize(pet.species);
-      clone.getElementById("button").textContent=`Adopt ${pet.name}`;
-      clone.getElementById("photo").setAttribute("src",`${pet.photo}`);
-      clone.getElementById("anchor").setAttribute("href",`${website}/pets/${pet.id}/`);
-      // Inserting clone
-      wrapper.appendChild(clone);
-    }
+    // Creating clone
+    const clone = template.content.cloneNode(true);
+    clone.getElementById("template").classList.add("clone");
+    clone.getElementById("template").classList.add("vis");
+    // Adapting clone
+    clone.getElementById("name").textContent=`${pet.name}`;
+    clone.getElementById("birthYear").textContent= petYears(pet.birthYear);
+    clone.getElementById("description").textContent=`${pet.description}`;
+    clone.getElementById("species").textContent= capitalize(pet.species);
+    clone.getElementById("button").textContent=`Adopt ${pet.name}`;
+    clone.getElementById("photo").setAttribute("src",`${pet.photo}`);
+    clone.getElementById("anchor").setAttribute("href",`${website}/pets/${pet.id}/`);
+    // Inserting clone
+    wrapper.appendChild(clone);
     }
   )
 }
